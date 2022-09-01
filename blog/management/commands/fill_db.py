db5ded39fc676d1e2832401709d0061d7c0ba4ba
David(gecert) <dkarandze4@gmail.com>
2022-09-01 10:54:46 +0200

    Create models and custom command for fill db

---
+++ blog/management/commands/fill_db.py
@@ -0,0 +1,43 @@
+from random import choice, randint
+
+from blog.models import Comment, Post
+
+from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+from faker import Faker
+
+fake = Faker()
+UserModel = get_user_model()
+
+
+class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('count', type=int, choices=range(10, 500), help='Number of records for Users'
+                                                                            '(from 10 to 500)')
+
+    def handle(self, *args, **options):
+        count = options['count']
+
+        for i in range(count):
+            User.objects.create(username=fake.name(), password=make_password('password'))
+
+        posts = []
+        for el in UserModel.objects.all():
+            for i in range(5):
+                posts.append(Post(title=fake.sentence(nb_words=2), brief_description=fake.sentence(nb_words=2),
+                                  full_description=fake.text(),
+                                  image=fake.image_url(width=randint(225, 825), height=randint(225, 825)),
+                                  posted=choice((True, False)), author=el))
+        Post.objects.bulk_create(posts)
+
+        comments = []
+        for el in Post.objects.all():
+            for i in range(7):
+                comments.append(Comment(username=fake.name(), text=fake.sentence(nb_words=4), post=el,
+                                        posted_com=choice((True, False))))
+        Comment.objects.bulk_create(comments)
+
+        self.stdout.write(self.style.SUCCESS('Successfully filled database'))
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,41 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=250)
+    brief_description = models.CharField(max_length=250)
+    full_description = models.CharField(max_length=500)
+    image = models.ImageField(null=True, blank=True)
+    posted = models.BooleanField(default=False)
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    username = models.CharField(max_length=250, default='David')
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    posted_com = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+
+class UserUrl(User):
+
+    def get_absolute_url(self):
+        return reverse('user-detail', kwargs={'pk': self.pk})
+
